cdb36f3b55ef5254d320908b2d98250b1a111fdb
tdongsi <dongsi.tuecuong@gmail.com>
2015-03-25 20:21:44 -0700

    Add unit tests for multiple input files from command line.

--- TestRunner/src/test/java/my/sqltest/AppTest.java
+++ TestRunner/src/test/java/my/sqltest/AppTest.java
@@ -28,7 +28,7 @@ public class AppTest {
 	}
 	
 	@Test
-	public void testCommandlineParamsFull() {
+	public void testCommandlineParams_Full() {
 		String[] argv = { "-username", "dbadmin", "-password", "password","-d", "jdbc:vertica://192.168.5.133:5433/VMart", "-file", "test.sql"};
 		App.AppParameter params = App.AppParameter.parseCommandLine(argv);
 		
@@ -39,13 +39,26 @@ public class AppTest {
 	}
 	
 	@Test
-	public void testCommandlineParamsMixed() {
+	public void testCommandlineParams_Mixed() {
 		String[] argv = { "-username", "dbadmin", "-password", "password","-d", "jdbc:vertica://192.168.5.133:5433/VMart", "-f", "test.sql"};
 		App.AppParameter params = App.AppParameter.parseCommandLine(argv);
 		
 		Assert.assertEquals(params.getUsername(), "dbadmin");
 		Assert.assertEquals(params.getPassword(), "password");
 		Assert.assertEquals(params.getInputfile(), Arrays.asList("test.sql"));
+		Assert.assertEquals(params.getUrl(), "jdbc:vertica://192.168.5.133:5433/VMart");
+	}
+	
+	@Test
+	public void testCommandlineParams_MultipleFiles() {
+		String[] argv = { "-u", "dbadmin", "-p", "password","-d", 
+				"jdbc:vertica://192.168.5.133:5433/VMart", 
+				"-f", "test.sql", "-f", "test2.sql", "-f", "my.test"};
+		App.AppParameter params = App.AppParameter.parseCommandLine(argv);
+		
+		Assert.assertEquals(params.getUsername(), "dbadmin");
+		Assert.assertEquals(params.getPassword(), "password");
+		Assert.assertEquals(params.getInputfile(), Arrays.asList("test.sql","test2.sql", "my.test"));
 		Assert.assertEquals(params.getUrl(), "jdbc:vertica://192.168.5.133:5433/VMart");
 	}
 }
