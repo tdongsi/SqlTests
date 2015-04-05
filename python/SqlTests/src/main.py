a7421b59fcb4a9af7cb775c52a34a451fbe3d0de
tdongsi <dongsi.tuecuong@gmail.com>
2015-04-04 23:07:15 -0700

    Add argument parsing to the main script.

--- python/SqlTests/src/main.py
+++ python/SqlTests/src/main.py
@@ -4,6 +4,8 @@ Created on Apr 4, 2015
 @author: cdongsi
 '''
 
+import argparse
+
 import logging
 import MyLogger
 myLogger = logging.getLogger('PySqlTest')
@@ -12,7 +14,40 @@ class SqlRunner(object):
     pass
 
 def main():
-    pass
+    '''
+    TODO
+    '''
+    
+    parser = argparse.ArgumentParser(description='Script to run SQL scripts ' 
+        'and annotated tests in it.')
+    
+    parser.add_argument('-database', action='store', required=True,
+                        dest='url', help='Test database\'s JDBC URL.')
+    
+    parser.add_argument('-username', action='store', dest='username',
+                        required=True,
+                        help='Login credentials.')
+    
+    parser.add_argument('-password', action='store', dest='password',
+                        required=True,
+                        help='Login credentials.')
+    
+    parser.add_argument('-file', action='store', dest='inputfile',
+                        required=True,
+                        help='SQL file to be tested.')
+    
+    # Vertica-specific optional argument.
+    parser.add_argument('-schema', action='store', dest='schema',
+                        required=False,
+                        help='Vertica schema to be tested.')
+    
+    args = parser.parse_args()
+    myLogger.debug("Database URL: %s", args.url)
+    myLogger.debug("Username: %s", args.username)
+    # DO NOT log password
+    myLogger.debug("SQL file: %s", args.inputfile)
+    # TODO: default value for schema
+    myLogger.debug("Schema name: %s", args.schema)
 
 if __name__ == '__main__':
     main()
