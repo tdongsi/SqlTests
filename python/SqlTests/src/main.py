a0e668b3ebd21f0cb672e100f1bc7567e42c45b7
tdongsi <dongsi.tuecuong@gmail.com>
2015-04-04 22:41:46 -0700

    Getting started on PySqlTest. Configure logging.

---
+++ python/SqlTests/src/main.py
@@ -0,0 +1,18 @@
+'''
+Created on Apr 4, 2015
+
+@author: cdongsi
+'''
+
+import logging
+import MyLogger
+myLogger = logging.getLogger('PySqlTest')
+
+class SqlRunner(object):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
